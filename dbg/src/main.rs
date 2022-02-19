32b34cbbc1719e6d0e53dd622d2e66d88765c2db
Niklas Jonsson <niklasandersjonsson@gmail.com>
2022-02-19 23:20:27 +0100

    WIP: sun simulation

--- dbg/src/main.rs
+++ dbg/src/main.rs
@@ -1,5 +1,5 @@
 use ramneryd::math;
-use ramneryd::math::Rgba;
+use ramneryd::math::{Quat, Rgb, Rgba, Vec3};
 use ramneryd::render;
 
 use ramneryd::common::Name;
@@ -86,6 +86,90 @@ impl Module for Args {
     }
 }
 
+#[derive(Component)]
+struct Sun {
+    angular_velocity: f32,
+    angle: f32,
+}
+
+const SUN_HEIGHT: f32 = 10.0;
+
+struct SunSimulation;
+
+struct SunMovement;
+
+impl SunMovement {
+    const ID: &'static str = "SunMovement";
+}
+
+impl<'a> System<'a> for SunMovement {
+    type SystemData = (
+        WriteStorage<'a, Sun>,
+        WriteStorage<'a, math::Transform>,
+        ReadExpect<'a, ramneryd::Time>,
+    );
+
+    fn run(&mut self, data: Self::SystemData) {
+        let (mut suns, mut transforms, time) = data;
+        for (sun, tfm) in (&mut suns, &mut transforms).join() {
+            let delta = time.delta_sim().as_secs();
+            sun.angle += delta * sun.angular_velocity;
+
+            let y = sun.angle.sin() * SUN_HEIGHT;
+            let x = sun.angle.cos() * SUN_HEIGHT;
+
+            if y < 0.0 {
+                sun.angular_velocity *= -1.0;
+            }
+
+            tfm.position.x = x;
+            tfm.position.y = y;
+
+            tfm.rotation = Quat::rotation_from_to_3d(
+                ramneryd::render::Light::DEFAULT_FACING,
+                Vec3::from(0.0) - tfm.position,
+            );
+        }
+    }
+}
+
+impl Module for SunSimulation {
+    fn load(&mut self, loader: &mut ModuleLoader) {
+        loader.add_system(SunMovement, SunMovement::ID, &[]);
+        loader
+            .world
+            .create_entity()
+            .with(Name::from("Sun"))
+            .with(math::Transform {
+                position: Vec3 {
+                    x: 0.0,
+                    y: SUN_HEIGHT,
+                    z: 0.0,
+                },
+                rotation: Quat::rotation_from_to_3d(
+                    ramneryd::render::Light::DEFAULT_FACING,
+                    Vec3::new(0.0, -1.0, 0.0),
+                ),
+                scale: 1.0,
+            })
+            .with(Sun {
+                angular_velocity: (3.0_f32).to_radians(),
+                angle: std::f32::consts::FRAC_PI_2,
+            })
+            .with(ramneryd::render::Light::Directional {
+                color: Rgb {
+                    r: 0.5,
+                    g: 0.5,
+                    b: 0.5,
+                },
+            })
+            .build();
+    }
+}
+
 fn main() {
-    ramneryd::Init::new().with_module(Args::parse()).run();
+    ramneryd::Init::new()
+        .with_module(Args::parse())
+        .with_module(SunSimulation)
+        .run();
 }
